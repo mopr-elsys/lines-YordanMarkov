b83e70faf4355b4578efc9a5da72221a4c0203f0
YordanMarkov <yordanmarkov2004@gmail.com>
2021-11-11 19:08:20 +0200

    Yordan Markov N15

--- geom.hpp
+++ geom.hpp
@@ -41,7 +41,7 @@ struct Line {
 
     bool parallel(const Line& other) const {
         //return eq(this->A/other.A, this->B/other.B) && this->C != other.C;
-        return eq(this->A*other.B, this->B*other.A) && this->C != other.C;
+        return eq(this->A*other.B, this->B*other.A) && this->A*other.C != other.A*this->C;
     }
 
     Line parallel(const Point& p) {
